b9aff70403d469fabaf99a80908a64c418d3c300
kazuma1989 <kazuma.ebina@quipper.com>
2020-11-29 14:50:36 +0900

    feat: ドロップダウンを置いてみた

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,34 @@ export function App() {
         <a className="btn btn-outline-primary" href="#">
           サインアップ
         </a>
+
+        <a
+          className="nav-link dropdown-toggle"
+          href="#"
+          id="navbarDropdownMenuLink"
+          role="button"
+          data-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Dropdown link
+        </a>
+        <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <li>
+            <a className="dropdown-item" href="#">
+              Action
+            </a>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Another action
+            </a>
+          </li>
+          <li>
+            <a className="dropdown-item" href="#">
+              Something else here
+            </a>
+          </li>
+        </ul>
       </div>
 
       <div
